8f8efb45977ff3f126d159409643c633d66c0d3e
Daia Bromberg <bromd514@newschool.edu>
2025-04-10 13:55:59 -0400

    mini save for pages

--- Project/vue-app/vue.config.js
+++ Project/vue-app/vue.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true,
+    disableHostCheck: true
   
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/msdv-thesis/'
-    : '/'
+    
   }
 }
 
